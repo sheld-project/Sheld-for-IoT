79a6b08a70f8003bf871e239b9f54fb78277ceb0
AkamaKousei <s16003@tokyo.kosen-ac.jp>
2017-04-27 00:44:09 +0900

    [add] add wifi server interface

--- Source/src/interface/wifi.cpp
+++ Source/src/interface/wifi.cpp
@@ -4,7 +4,9 @@ Wi-Fiの制御＆インターフェース.
 
 #include <string>
 #include <map>
-// #include <ESP8266WiFi.h>
+#include <iostream>
+
+#include <ESP8266WiFi.h>
 
 
 /* --- Wi-Fi制御クラス --- */
@@ -18,6 +20,9 @@ private:
       {"PASS",""},
       {"HOST",""}
   };
+  WiFiServer server = WiFiServer(80);
+  WiFiClient client;
+  int status = WL_IDLE_STATUS;
 
 public:
   /* プロパティの初期化 */
@@ -30,5 +35,46 @@ public:
     CONNECTION_PROPERTY["SSID"] = arg_ssid;
     CONNECTION_PROPERTY["PASS"] = arg_password;
     CONNECTION_PROPERTY["HOST"] = arg_host;
+  }
+
+  /* Wi-Fiに接続する */
+  bool ConnectWifi()
+  {
+    // 接続
+    status = WiFi.begin(
+      CONNECTION_PROPERTY["SSID"].c_str(),
+      CONNECTION_PROPERTY["PASS"].c_str()
+    );
+
+    // 接続できたらTrue,失敗したらFalseを返す
+    if ( status == WL_CONNECTED)
+    {
+      server.begin();
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  /* クライアントを待つ */
+  void WaitClient()
+  {
+        client = server.available();
+  }
+
+  /* TCPストリームを行うクラス */
+  std::string TcpStream(){
+    // result
+    std::string response = "";
+
+    // クライアントが来たら、受け取ったバイト列の長さが100になるまで受け取る
+    while(response.length() < 100)
+    {
+      response += client.read();
+    }
+
+    return response;
   }
 };
