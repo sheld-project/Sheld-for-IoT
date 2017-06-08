373b987bb2b0a71359f211daf63cda1a91bca011
akakou <akakou571@gmail.com>
2017-06-09 04:46:46 +0900

    [add] SDに関する一連の動作を作成

--- Source/src/interface/microsd.cpp
+++ Source/src/interface/microsd.cpp
@@ -8,12 +8,40 @@
 class SD_
 {
 private:
-  File path;
+  File file;
+  std::string file_body;
 
 public:
-  SD_(std::string arg_ssid_str)
+  /* ファイルパス等の設定 */
+  SD_(std::string path)
   {
-    // パスの設定
-    path = SD.open(arg_ssid_str.c_str());
+    file = SD.open(path.c_str());
+  }
+
+  /* SD カードとの接続待ち*/
+  bool connectSD()
+  {
+    if (file) {
+      return true;
+    } else {
+    // if the file didn't open, print an error:
+    return false;
+    }
+  }
+
+  /* ファイルを文字列として読み込む */
+  std::string readfFile()
+  {
+    while (file.available())
+    {
+      file.read();
+    }
+    return file_body;
+  }
+
+  /* SDカードの遮断 */
+  void close()
+  {
+    file.close();
   }
 };
