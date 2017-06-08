ebdbc7a6f4b94484fe3c36eec1ce1a37153eb8ab
akakou <akakou571@gmail.com>
2017-06-09 04:46:46 +0900

    [add] SDに関する一連の動作を作成

--- Source/src/main.cpp
+++ Source/src/main.cpp
@@ -3,19 +3,35 @@
 "Shellder" のメインソースコード.
 */
 
+#include <string>
 #include <Arduino.h>
 
 #include "./interface/wifi.cpp"
+#include "./interface/microsd.cpp"
 
 
 WiFi_ *wifi;
+SD_ *sd;
+std::string config;
 
 void setup()
 {
   Serial.begin(9600);
 
-  // 接続
+  // SD接続
+  sd = new SD_("/.config");
+  while (!sd->connectSD())  // 接続待ち
+
+  // SD読み取り
+  config = sd->readfFile();
+
+  // 接続を切る
+  sd->close();
+  delete sd;
+
+  // Wifi接続
   wifi = new WiFi_("ssid","pass");
+
   wifi->connectWifi();
 
   Serial.print("Connect");
@@ -25,7 +41,7 @@ void setup()
   Serial.println(ip);
 
   // サーバの立ち上げ
-  wifi->set_server();
+  wifi->setServer();
   Serial.print("start");
 }
 
